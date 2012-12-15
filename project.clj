4089c3ada010c371d4a0532d193b93d03d2b3ad3
Carlos Vilhena <carlosvilhena@gmail.com>
2012-12-15 22:34:03 +0000

    first commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject www-4clojure "0.1.0-SNAPSHOT"
+  :description "Exercises from www.4clojure.com"
+  :url "http://www.4clojure.com"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]])
---
+++ test/www_4clojure/core_test.clj
@@ -0,0 +1,7 @@
+(ns www-4clojure.core-test
+  (:use clojure.test
+        www-4clojure.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
