a0aa503b9886a1c61a400c8d7540780efd2b212d
Carlos Vilhena <carlosvilhena@gmail.com>
2012-12-15 22:49:27 +0000

    solution for problem 20

--- src/www_4clojure/core.clj
+++ src/www_4clojure/core.clj
@@ -4,3 +4,8 @@
   "Problem 19"
   [x]
   (first (drop (- (count x) 1) x)))
+
+(defn penultimate-element
+  "Problem 20"
+  [l]
+  (last (take (- (count l) 1) l)))
--- test/www_4clojure/core_test.clj
+++ test/www_4clojure/core_test.clj
@@ -6,3 +6,8 @@
   (= (last-element [1 2 3 4 5]) 5) => true
   (= (last-element '(5 4 3)) 3) => true
   (= (last-element ["b" "c" "d"]) "d") => true)
+
+(fact "Problem 20 - Penultimate Element"
+  (= (penultimate-element (list 1 2 3 4 5)) 4) => true
+     (= (penultimate-element ["a" "b" "c"]) "b") => true
+     (= (penultimate-element [[1 2] [3 4]]) [1 2]) => true)
